7790771e6c1fa7c56d7968bf7ea9238962d8b61b
Mahmoud Samir <menatosamir55@gmail.com>
2024-01-22 13:44:07 +0200

    Finish Lift-off Part3

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,5 +1,6 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 import { TrackModel, AuthorModel } from "../models"
+import { Module } from "../types";
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
@@ -10,5 +11,13 @@ export class TrackAPI extends RESTDataSource {
 
   getAuthor(authorId: string) {
     return this.get<AuthorModel>(`author/${authorId}`);
+  }
+
+  getTrack(trackId: string) {
+    return this.get<TrackModel>(`track/${trackId}`);
+  }
+
+  getTrackModules(trackId: string) {
+    return this.get<Module[]>(`track/${trackId}/modules`);
   }
 }
